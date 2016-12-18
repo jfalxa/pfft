760aea2acfbe37a9b27ea5d439639941ba59a5f0
jfalxa <jfalxa.sta@gmail.com>
2016-12-18 19:02:06 +0100

    Resolve module path only inside pfft.run

--- packages/pfft/src/run.js
+++ packages/pfft/src/run.js
@@ -1,10 +1,12 @@
+const path       = require( 'path' );
 const findPreset = require( './utils/findPreset' );
 
 
 module.exports = function run( script, config )
 {
-    const preset    = findPreset();
-    const runScript = require( `${ preset }/scripts/${ script }` );
+    const preset     = findPreset();
+    const scriptPath = path.resolve( `node_modules/${ preset }/scripts/${ script }` );
+    const runScript  = require( scriptPath );
 
     return runScript();
 }
--- packages/pfft/src/utils/findPreset.js
+++ packages/pfft/src/utils/findPreset.js
@@ -9,7 +9,7 @@ module.exports = function findPreset()
     {
         if ( module.indexOf( 'pfft-preset-' ) === 0 )
         {
-            return path.resolve( `node_modules/${ module }` );
+            return module;
         }
     }
 
