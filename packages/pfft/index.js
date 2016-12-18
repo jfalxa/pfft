cc13cec17d3c5ed0ef5cefb13be3a03ad93623e4
jfalxa <jfalxa.sta@gmail.com>
2016-12-18 18:15:45 +0100

    Added description to current pfft commands

--- packages/pfft/index.js
+++ packages/pfft/index.js
@@ -8,9 +8,11 @@ programm.version( '1.0.0' )
     .description( 'pfft aims at centralizing boilerplate code.' );
 
 programm.command( 'init <preset> [path]' )
+    .description( 'fetch the given preset and initialize boilerplate code' )
     .action( init );
 
 programm.command( 'run <script> [config]' )
+    .description( 'execute the specified script of the current preset' )
     .action( run );
 
 
