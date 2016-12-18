906572aa303ecb3915d3394f638b454b08d303bd
jfalxa <jfalxa.sta@gmail.com>
2016-12-18 01:25:01 +0100

    Removed left overs causing crash

--- packages/pfft/index.js
+++ packages/pfft/index.js
@@ -9,14 +9,5 @@ programm.version( '1.0.0' )
 programm.command( 'init <preset> [path]' )
     .action( init );
 
-programm.command( 'start [config]' )
-    .action( start );
-
-programm.command( 'build [config]' )
-    .action( build );
-
-programm.command( 'test [config]' )
-    .action( test );
-
 
 module.exports = programm.parse( process.argv );
