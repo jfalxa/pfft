c1ff2b1fa7d468b28b5c4dc2c8311261e89eb165
jfalxa <jfalxa.sta@gmail.com>
2016-12-21 23:53:23 +0100

    Added ava babel preset to react preset

--- packages/pfft-partial-ava/scripts/coverage.sh
+++ packages/pfft-partial-ava/scripts/coverage.sh
@@ -3,5 +3,10 @@ cross_env="./node_modules/pfft-partial-ava/node_modules/.bin/cross-env"
 ava="./node_modules/pfft-partial-ava/node_modules/.bin/ava"
 nyc="./node_modules/pfft-partial-ava/node_modules/.bin/nyc"
 
+NYC_OPTIONS="
+    --require babel-register
+    --instrument false
+    --source-map false"
+
 # run tests with optional arguments
-$cross_env NODE_ENV=coverage $nyc $ava $@
+$cross_env NODE_ENV=coverage $nyc $NYC_OPTIONS $ava $@
