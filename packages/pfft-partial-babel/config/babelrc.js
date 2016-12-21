748852ec64ff6f3a4c8b7318ed3c57232402a12d
jfalxa <jfalxa.sta@gmail.com>
2016-12-21 17:56:10 +0100

    Use presets in babel config instead of extends

---
+++ packages/pfft-partial-babel/config/babelrc.js
@@ -0,0 +1,18 @@
+module.exports =
+{
+    presets :
+    [
+        'es2015',
+        'stage-0'
+    ],
+
+    plugins :
+    [
+        [
+            'module-resolver',
+            {
+                root : ['.']
+            }
+        ]
+    ]
+}
---
+++ packages/pfft-preset-react-app/config/babelrc.js
@@ -0,0 +1,18 @@
+module.exports =
+{
+    presets :
+    [
+        'react'
+    ],
+
+    env :
+    {
+        development :
+        {
+            plugins :
+            [
+                'react-hot-loader/babel'
+            ]
+        }
+    }
+}
