9e2c58b6871f663a5a3446ee7f0517bbf6c3c911
jfalxa <jfalxa.sta@gmail.com>
2016-12-21 18:26:00 +0100

    Execute scripts with sh in react preset

--- packages/pfft-preset-react-app/scripts/build.sh
+++ packages/pfft-preset-react-app/scripts/build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-build="./node_modules/pfft-partial-webpack/scripts/build.sh"
+build="sh ./node_modules/pfft-partial-webpack/scripts/build.sh"
 prod_config="./node_modules/pfft-preset-react-app/webpack/production.js"
 
 # compile js and css for production
--- packages/pfft-preset-react-app/scripts/start.sh
+++ packages/pfft-preset-react-app/scripts/start.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-start="./node_modules/pfft-partial-webpack/scripts/start.sh"
+start="sh ./node_modules/pfft-partial-webpack/scripts/start.sh"
 dev_config="./node_modules/pfft-preset-react-app/webpack/development.js"
 
 # srart dev server
