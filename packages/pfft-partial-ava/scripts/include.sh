d65a8dfb29816050a581b8bf5061e7ebe1cde694
jfalxa <jfalxa.sta@gmail.com>
2016-12-22 03:33:51 +0100

    debugggg

--- packages/pfft-partial-ava/scripts/include.sh
+++ packages/pfft-partial-ava/scripts/include.sh
@@ -3,7 +3,7 @@ PARTIAL="./node_modules/pfft-partial-ava"
 json="$PARTIAL/node_modules/.bin/json"
 AVARC="$PARTIAL/config/avarc.json"
 
-cat package.json $AVARC
-
 # merge ava config inside package.json
-cat package.json $AVARC | $json --merge > package2.json
+cat package.json $AVARC | $json --merge > package.json
+
+sleep 1
--- packages/pfft/src/utils/updatePackage.js
+++ packages/pfft/src/utils/updatePackage.js
@@ -7,12 +7,12 @@ module.exports = function udpatePackageJSON( path, update )
     const packagePath = `${ path }/package.json`;
 
     // get the current package.json and apply the update
-    const packageJSON        = require( packagePath );
+    const packageJSON        = require( packagePath ); console.log(packageJSON)
     const updatedPackageJSON = merge( packageJSON, update );
 
     // preprare the package.json contents
     const packageJSONStr = JSON.stringify( updatedPackageJSON, null, '  ' );
-
+console.log(packageJSONStr)
     // and write them in the file
     fs.writeFileSync( packagePath, packageJSONStr );
 }
