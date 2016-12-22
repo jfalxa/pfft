3ea327c1d205969ad42c54dea0c722f01770d951
jfalxa <jfalxa.sta@gmail.com>
2016-12-22 16:13:23 +0100

    Added ability to pass custom config to build scripts

--- packages/pfft-preset-react-app/scripts/build.sh
+++ packages/pfft-preset-react-app/scripts/build.sh
@@ -3,8 +3,12 @@ build="sh ./node_modules/pfft-partial-webpack/scripts/build.sh"
 clean="sh ./node_modules/pfft-preset-react-app/scripts/clean.sh"
 prod_config="./node_modules/pfft-preset-react-app/webpack/production.js"
 
+# prepare config with optional args
+# fallback to prod config if nothing specified
+options=${@:-$prod_config}
+
 # clean output folder
 $clean
 
 # compile js and css for production
-$build --config $prod_config
+$build --config $options
--- packages/pfft-preset-react-app/scripts/start.sh
+++ packages/pfft-preset-react-app/scripts/start.sh
@@ -3,8 +3,12 @@ start="sh ./node_modules/pfft-partial-webpack/scripts/start.sh"
 clean="sh ./node_modules/pfft-preset-react-app/scripts/clean.sh"
 dev_config="./node_modules/pfft-preset-react-app/webpack/development.js"
 
+# prepare config with optional args
+# fallback to dev config if nothing specified
+options=${@:-$dev_config}
+
 # clean output folder
 $clean
 
-# srart dev server
-$start --config $dev_config
+# start dev server
+$start --config $options
