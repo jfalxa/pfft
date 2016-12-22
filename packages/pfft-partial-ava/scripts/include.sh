20f40b17fe992b03f2ecce6e34b608ca490e5ddc
jfalxa <jfalxa.sta@gmail.com>
2016-12-22 03:06:33 +0100

    Add extra line to package.json before merging it with ava config

--- packages/pfft-partial-ava/scripts/include.sh
+++ packages/pfft-partial-ava/scripts/include.sh
@@ -3,10 +3,8 @@ PARTIAL="./node_modules/pfft-partial-ava"
 json="$PARTIAL/node_modules/.bin/json"
 AVARC="$PARTIAL/config/avarc.json"
 
-echo '--------------------------------------------------------'
-pwd
-cat package.json
-echo '--------------------------------------------------------'
+# add extra line to package.json for the merging to succeed
+echo "\n" >> package.json
 
 # merge ava config inside package.json
 cat package.json $AVARC | $json --merge > package.json
