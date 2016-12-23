5e375352b043f8a1f5ffad4860951d099e89ad21
jfalxa <jfalxa.sta@gmail.com>
2016-12-23 16:52:31 +0100

    Add some convention in var naming in sh scripts

--- packages/pfft-preset-react-app/scripts/build.sh
+++ packages/pfft-preset-react-app/scripts/build.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 build="sh ./node_modules/pfft-partial-webpack/scripts/build.sh"
 clean="sh ./node_modules/pfft-preset-react-app/scripts/clean.sh"
-prod_config="./node_modules/pfft-preset-react-app/webpack/production.js"
+PROD_CONFIG="./node_modules/pfft-preset-react-app/webpack/production.js"
 
 # prepare config with optional args
 # fallback to prod config if nothing specified
-options=${@:-$prod_config}
+OPTIONS=${@:-$PROD_CONFIG}
 
 # clean output folder
 $clean
 
 # compile js and css for production
-$build --config $options
+$build --config $OPTIONS
--- packages/pfft-preset-react-app/scripts/start.sh
+++ packages/pfft-preset-react-app/scripts/start.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 start="sh ./node_modules/pfft-partial-webpack/scripts/start.sh"
 clean="sh ./node_modules/pfft-preset-react-app/scripts/clean.sh"
-dev_config="./node_modules/pfft-preset-react-app/webpack/development.js"
+DEV_CONFIG="./node_modules/pfft-preset-react-app/webpack/development.js"
 
 # prepare config with optional args
 # fallback to dev config if nothing specified
-options=${@:-$dev_config}
+OPTIONS=${@:-$DEV_CONFIG}
 
 # clean output folder
 $clean
 
 # start dev server
-$start --config $options
+$start --config $OPTIONS
