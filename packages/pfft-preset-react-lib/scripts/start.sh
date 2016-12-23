db0b5c0dd906d1a763f7742ebd5a1930510a47d3
jfalxa <jfalxa.sta@gmail.com>
2016-12-23 18:03:04 +0100

    Hide storybook config in react-lib preset

--- packages/pfft-preset-react-lib/scripts/start.sh
+++ packages/pfft-preset-react-lib/scripts/start.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 start_storybook="./node_modules/pfft-preset-react-lib/node_modules/.bin/start-storybook"
+STORYBOOK_CONFIG="./node_modules/pfft-preset-react-lib/.storybook"
 
 # start storybook dev server
-$start_storybook -p 3000 -c .storybook
+$start_storybook -p 3000 -c $STORYBOOK_CONFIG
