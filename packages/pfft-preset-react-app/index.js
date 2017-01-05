9846349879d0ac586163ea258fbc8c44485081fa
jfalxa <jfalxa.sta@gmail.com>
2017-01-05 17:13:31 +0100

    Simplified script proxying in react presets

--- packages/pfft-preset-react-app/index.js
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env node
-const proxyScripts = require( 'pfft-utils/proxy-scripts' );
-module.exports = proxyScripts( 'react-app', process.argv );
--- packages/pfft-preset-react-lib/index.js
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env node
-const proxyScripts = require( 'pfft-utils/proxy-scripts' );
-module.exports = proxyScripts( 'react-lib', process.argv );
